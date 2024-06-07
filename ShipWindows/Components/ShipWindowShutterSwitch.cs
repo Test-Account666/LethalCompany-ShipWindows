61f2b90db853a3b23daaee47b59808f3d086da46
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2024-06-07 07:03:41 +0200

    Fix locked windows not actually being locked

--- ShipWindows/Components/ShipWindowShutterSwitch.cs
+++ ShipWindows/Components/ShipWindowShutterSwitch.cs
@@ -8,13 +8,18 @@ namespace ShipWindows.Components;
 [AddComponentMenu("TestAccount666/ShipWindows/ShipWindowShutterSwitch")]
 public class ShipWindowShutterSwitch : NetworkBehaviour {
     public InteractTrigger interactTrigger;
-    
+
     public override void OnNetworkSpawn() {
         base.OnNetworkSpawn();
 
         interactTrigger.onInteract.AddListener(PlayerUsedSwitch);
     }
 
-    public void PlayerUsedSwitch(PlayerControllerB playerControllerB) =>
+    public void PlayerUsedSwitch(PlayerControllerB playerControllerB) {
         NetworkHandler.WindowSwitchUsed(WindowState.Instance.windowsClosed);
+
+        if (!WindowState.Instance.windowsLocked) return;
+
+        NetworkHandler.WindowSwitchUsed(WindowState.Instance.windowsClosed);
+    }
 }
