8fad78c569b06503f0ab1ac26d8b3cff89019500
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2024-06-07 14:26:29 +0200

    Make shutter switch non-interactable when windows are locked

--- ShipWindows/Components/ShipWindowShutterSwitch.cs
+++ ShipWindows/Components/ShipWindowShutterSwitch.cs
@@ -12,11 +12,38 @@ public class ShipWindowShutterSwitch : NetworkBehaviour {
     public InteractTrigger interactTrigger;
     public Animator animator;
     private static readonly int _OnHash = Animator.StringToHash("on");
+    private bool destroy;
 
     public override void OnNetworkSpawn() {
         base.OnNetworkSpawn();
 
         interactTrigger.onInteract.AddListener(PlayerUsedSwitch);
+
+        StartCoroutine(SyncInteractable());
+    }
+
+    public override void OnNetworkDespawn() {
+        destroy = true;
+
+        base.OnNetworkDespawn();
+    }
+
+    public override void OnDestroy() {
+        destroy = true;
+
+        base.OnDestroy();
+    }
+
+    private IEnumerator SyncInteractable() {
+        var currentlyLocked = WindowState.Instance.windowsLocked;
+
+        yield return new WaitUntil(() => destroy || WindowState.Instance.windowsLocked != currentlyLocked);
+
+        if (destroy) yield break;
+
+        interactTrigger.interactable = !WindowState.Instance.windowsLocked;
+
+        StartCoroutine(SyncInteractable());
     }
 
     public void PlayerUsedSwitch(PlayerControllerB playerControllerB) {
