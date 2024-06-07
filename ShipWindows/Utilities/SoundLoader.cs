30c9cc2a3e49462fc624db76807ce8ebbe246e72
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2024-06-07 23:14:32 +0200

    Add SoundLoader.cs to load VoiceLines

---
+++ ShipWindows/Utilities/SoundLoader.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Networking;
+using Debug = System.Diagnostics.Debug;
+
+namespace ShipWindows.Utilities;
+
+public static class SoundLoader {
+    public static readonly AudioClip[] VoiceLines = new AudioClip[2];
+
+    public static IEnumerator LoadAudioClips() {
+        var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        Debug.Assert(assemblyDirectory != null, nameof(assemblyDirectory) + " != null");
+        var audioPath = Path.Combine(assemblyDirectory, "sounds");
+
+        audioPath = Directory.Exists(audioPath)? audioPath : Path.Combine(assemblyDirectory);
+
+        ShipWindows.Logger.LogInfo("Loading Wesley voice lines...");
+
+        var voiceLinesAudioPath = Path.Combine(audioPath, "voicelines");
+
+        voiceLinesAudioPath = Directory.Exists(voiceLinesAudioPath)? voiceLinesAudioPath : Path.Combine(assemblyDirectory);
+
+        LoadShutterCloseClip(voiceLinesAudioPath);
+
+        LoadShutterOpenClip(voiceLinesAudioPath);
+
+        yield break;
+    }
+
+    private static void LoadShutterOpenClip(string voiceLinesAudioPath) {
+        var shutterOpenFile = Path.Combine(voiceLinesAudioPath, "ShutterOpen.wav");
+
+        var shutterOpenFileName = Path.GetFileName(shutterOpenFile);
+
+        var shutterOpenVoiceLineAudioClip = LoadAudioClipFromFile(new(shutterOpenFile), shutterOpenFileName[..^4]);
+
+        if (shutterOpenVoiceLineAudioClip is null) {
+            ShipWindows.Logger.LogError("Failed to load voice line 'ShutterOpen'!");
+        } else {
+            VoiceLines[1] = shutterOpenVoiceLineAudioClip;
+            ShipWindows.Logger.LogInfo($"Loaded line '{shutterOpenVoiceLineAudioClip.name}'!");
+        }
+    }
+
+    private static void LoadShutterCloseClip(string voiceLinesAudioPath) {
+        var shutterCloseFile = Path.Combine(voiceLinesAudioPath, "ShutterClose.wav");
+
+        var shutterCloseFileName = Path.GetFileName(shutterCloseFile);
+
+        var shutterCloseVoiceLineAudioClip = LoadAudioClipFromFile(new(shutterCloseFile), shutterCloseFileName[..^4]);
+
+        if (shutterCloseVoiceLineAudioClip is null) {
+            ShipWindows.Logger.LogError("Failed to load voice line 'ShutterClose'!");
+            return;
+        }
+
+        VoiceLines[0] = shutterCloseVoiceLineAudioClip;
+        ShipWindows.Logger.LogInfo($"Loaded line '{shutterCloseVoiceLineAudioClip.name}'!");
+    }
+
+    private static AudioClip? LoadAudioClipFromFile(Uri filePath, string name) {
+        using var unityWebRequest = UnityWebRequestMultimedia.GetAudioClip(filePath, AudioType.WAV);
+
+        var asyncOperation = unityWebRequest.SendWebRequest();
+
+        while (!asyncOperation.isDone)
+            Thread.Sleep(100);
+
+        if (unityWebRequest.result != UnityWebRequest.Result.Success) {
+            ShipWindows.Logger.LogError("Failed to load AudioClip: " + unityWebRequest.error);
+            return null;
+        }
+
+        var clip = DownloadHandlerAudioClip.GetContent(unityWebRequest);
+
+        clip.name = name;
+
+        return clip;
+    }
+}
