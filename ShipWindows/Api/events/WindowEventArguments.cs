164440947e2ec1a2f42895d2faaff8f10025a084
TestAccount666 <36412486+Test-Account666@users.noreply.github.com>
2025-02-07 22:32:04 +0100

    Add windowObject variable for post-event

--- ShipWindows/Api/events/WindowEventArguments.cs
+++ ShipWindows/Api/events/WindowEventArguments.cs
@@ -1,6 +1,9 @@
+using UnityEngine;
+
 namespace ShipWindows.Api.events;
 
-public struct WindowEventArguments(WindowInfo windowInfo) {
+public struct WindowEventArguments(WindowInfo windowInfo, GameObject? windowObject = null) {
     public bool cancelled = false;
     public WindowInfo windowInfo = windowInfo;
+    public GameObject? windowObject = windowObject;
 }
