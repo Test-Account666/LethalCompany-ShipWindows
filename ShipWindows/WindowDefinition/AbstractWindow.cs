a9b338d9287919f45bc79d1a21705c5bf76bb14f
TestAccount666 <36412486+Test-Account666@users.noreply.github.com>
2025-02-07 22:30:52 +0100

    Fix layer change making window invisible

--- ShipWindows/WindowDefinition/AbstractWindow.cs
+++ ShipWindows/WindowDefinition/AbstractWindow.cs
@@ -8,18 +8,18 @@ public abstract class AbstractWindow : MonoBehaviour {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     public WindowInfo windowInfo;
     public MeshRenderer meshRenderer;
+    public Collider collider;
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
     private void Awake() {
         UpdateMaterial();
         WindowConfig.glassMaterial.SettingChanged += (_, _) => UpdateMaterial();
 
-        //TODO: Do we actually want this client-side?
         UpdateLayer();
         WindowConfig.allowEnemyTriggerThroughWindows.SettingChanged += (_, _) => UpdateLayer();
     }
 
-    public virtual void UpdateLayer() => gameObject.layer = LayerMask.NameToLayer(WindowConfig.allowEnemyTriggerThroughWindows.Value? "Railing" : "Room");
+    public virtual void UpdateLayer() => collider.gameObject.layer = LayerMask.NameToLayer(WindowConfig.allowEnemyTriggerThroughWindows.Value? "Railing" : "Room");
 
     public virtual void UpdateMaterial() => meshRenderer.material = WindowConfig.glassMaterial.Value.GetMaterial();
 
