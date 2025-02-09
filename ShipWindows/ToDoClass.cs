bdf0cfacfa1cb029ba7e681ae73ae0d75889fb6e
TestAccount666 <36412486+Test-Account666@users.noreply.github.com>
2025-02-09 16:13:24 +0100

    Remove todos

--- ShipWindows/ToDoClass.cs
+++ ShipWindows/ToDoClass.cs
@@ -1,9 +1,6 @@
 namespace ShipWindows;
 
 public class ToDoClass {
-    //TODO: Enemy Mesh Visibility
-    //TODO: Window Unlock Networking
     //TODO: Shutter Switch
-    //TODO: Skybox
     //TODO: Documentation
 }
