d83cee7f5b9692933f99a389d139ad6bfaa21c8c
TestAccount666 <36412486+Test-Account666@users.noreply.github.com>
2025-02-14 19:17:24 +0100

    Force don't move posters with other ship mods

--- ShipWindows/SoftDependencies/DependencyChecker.cs
+++ ShipWindows/SoftDependencies/DependencyChecker.cs
@@ -1,9 +1,32 @@
+using System.Collections.Generic;
 using System.Linq;
 using BepInEx.Bootstrap;
 
 namespace ShipWindows.SoftDependencies;
 
 public static class DependencyChecker {
-    public static bool IsCelestialTintInstalled() =>
-        Chainloader.PluginInfos.Values.Any(metadata => metadata.Metadata.GUID.Contains("CelestialTint"));
+    private static readonly Dictionary<string, bool> _DependencyDictionary = [
+    ];
+
+    public static bool thereIsAShipModInstalled;
+
+    public static bool IsCelestialTintInstalled() => IsDependencyInstalled("CelestialTint");
+
+    public static bool IsWiderShipInstalled() => IsDependencyInstalled("mborsh.WiderShipMod");
+
+    public static bool IsTwoStoryShipInstalled() => IsDependencyInstalled("MelanieMelicious.2StoryShip");
+
+    public static bool IsAnyShipModInstalled() => IsTwoStoryShipInstalled() || IsWiderShipInstalled() || thereIsAShipModInstalled;
+
+    public static bool IsDependencyInstalled(string dependencyName) {
+        var containsKey = _DependencyDictionary.TryGetValue(dependencyName, out var installed);
+
+        if (containsKey) return installed;
+
+        installed = Chainloader.PluginInfos.Values.Any(metadata => metadata.Metadata.GUID.Contains(dependencyName));
+
+        _DependencyDictionary[dependencyName] = installed;
+
+        return installed;
+    }
 }
