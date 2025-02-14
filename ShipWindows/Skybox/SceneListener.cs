0d9417a5872f1200d6fd459fe60f2dd12ba53220
TestAccount666 <36412486+Test-Account666@users.noreply.github.com>
2025-02-14 19:05:58 +0100

    Remove debug messages

--- ShipWindows/Skybox/SceneListener.cs
+++ ShipWindows/Skybox/SceneListener.cs
@@ -12,20 +12,14 @@ public class SceneListener {
     public SceneListener(AbstractSkyBox skyBox) {
         _skyBox = skyBox;
 
-        ShipWindows.Logger.LogFatal("Starting SceneListener!");
-
         SceneManager.sceneLoaded += (_, _) => {
             if (!StartOfRound.Instance) return;
-
-            ShipWindows.Logger.LogFatal("Scene loaded!");
 
             StartOfRound.Instance.StartCoroutine(CheckSceneStateDelayed());
         };
 
         SceneManager.sceneUnloaded += _ => {
             if (!StartOfRound.Instance) return;
-
-            ShipWindows.Logger.LogFatal("Scene unloaded!");
 
             StartOfRound.Instance.StartCoroutine(CheckSceneStateDelayed());
         };
@@ -38,23 +32,18 @@ public class SceneListener {
     }
 
     private void CheckSceneState() {
-        if (!ShipWindows.skyBox) {
-            ShipWindows.Logger.LogFatal("Skybox null?!");
-            return;
-        }
+        if (!ShipWindows.skyBox) return;
 
         if (SceneManager.sceneCount is not 1 || SceneManager.GetActiveScene() is not {
                 name: "SampleSceneRelay",
             }) {
             ShipWindows.skyBox!.ToggleSkyBox(false);
             EventAPI.AfterSkyboxUnloaded(_skyBox);
-            ShipWindows.Logger.LogFatal("Toggled skybox off!");
             return;
         }
 
         ShipWindows.skyBox!.ToggleSkyBox(true);
         EventAPI.AfterSkyboxLoaded(_skyBox);
-        ShipWindows.Logger.LogFatal("Toggled skybox on!");
 
         if (!WindowConfig.hideSpaceProps.Value) return;
         StartOfRound.Instance.currentPlanetPrefab.transform.parent.gameObject.SetActive(false);
